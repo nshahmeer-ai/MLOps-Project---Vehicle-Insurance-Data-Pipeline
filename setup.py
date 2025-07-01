9373a02b0c30ff5f00b8c5367fc0b3eb91a8e24c
Shahmeer Nawaz <shahmeernawazai@gmail.com>
2025-07-02 00:29:41 +0500

    project struture and env ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shahmeer Nawaz",
+    author_email="shahmeernawazai@gmail.com",
+    packages=find_packages()
+)
